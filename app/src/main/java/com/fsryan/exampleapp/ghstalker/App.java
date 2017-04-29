642026e57dcb077b76c2b664fd489831bd90308a
Ryan Scott <ryansgot@gmail.com>
2017-04-29 00:48:33 -0700

    leak canar yintegration with gradle property switch; crashlytics and answers property switches

--- app/src/main/java/com/fsryan/exampleapp/ghstalker/App.java
+++ app/src/main/java/com/fsryan/exampleapp/ghstalker/App.java
@@ -4,17 +4,42 @@ import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.answers.Answers;
+import com.squareup.leakcanary.LeakCanary;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.fabric.sdk.android.Fabric;
+import io.fabric.sdk.android.Kit;
 
 public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-        if (BuildConfig.FABRIC_ENABLED) {
-            Fabric.with(this, new Crashlytics(), new Answers());
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
         }
+        if (BuildConfig.LEAK_CANARY_ENABLED) {
+            LeakCanary.install(this);
+        }
+
+        startFabric(this);
+    }
+
+    private static void startFabric(App app) {
+        List<Kit> kits = new ArrayList<>();
+        if (BuildConfig.CRASHLYTICS_ENABLED) {
+            kits.add(new Crashlytics());
+        }
+        if (BuildConfig.ANSWERS_ENABLED) {
+            kits.add(new Answers());
+        }
+        if (kits.isEmpty()) {
+            return;
+        }
+        Fabric.with(app, kits.toArray(new Kit[0]));
     }
 }
